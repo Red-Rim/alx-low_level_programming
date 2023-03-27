3a8116bd5f75fff4d9a19135672b187163a840d8
Red-Rim <salmaouakhir20@gmail.com>
2023-03-27 14:15:09 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - print a string
+ * @str: the string
+ *
+ * Return: the length of the string
+ */
+
+void _puts(char *str)
+{
+	int j = 0
+		while (str[j] != '\0')
+		{
+			_putchar(str[j]);
+			j++;
+		}
+	_putchar('\n');
+}
