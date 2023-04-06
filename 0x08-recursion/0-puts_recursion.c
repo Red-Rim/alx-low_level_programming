a5b9c1d4953cea6577b7150facf24be6ef0ffce1
Red-Rim <salmaouakhir20@gmail.com>
2023-04-05 17:20:59 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string in reverse
+ * @s: string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s++);
+}
+else
+{
+	_putchar('\n');
+}
+}
