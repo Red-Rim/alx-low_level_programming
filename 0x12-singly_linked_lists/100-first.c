174c3dfc974c25302fa0ccfddfb2c4c6e34f88ee
Red-Rim <salmaouakhir20@gmail.com>
2023-04-27 19:56:46 -0700

    Luna

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists_t"
+#include <stdio.h>
+
+/*Red-Rim*/
+/**
+ * first - a function that prints before the main is executed
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
