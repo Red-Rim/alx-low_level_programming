159aa579d5ab5b438289568467c7680b30e18472
Red-Rim <salmaouakhir20@gmail.com>
2023-04-29 11:45:31 -0700

    0-main.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,27 +1,30 @@
 #include "main.h"
 /*Red-Rim*/
 /**
+ * sqrt_loop - returns the natural square using a loop
+ * @n: the number whose square root is to be computed
+ * @i: the current value to check
+ * Return: natural square root n or -1 if n doesn't have a natural square root
+ */
+int sqrt_loop(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	else
+		return (sqrt_loop(n, i + 1));
+}
+
+/**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: the number whose square root is to be computed
  * Return: natural square root n or -1 if n doesn't have a natural square root
  */
 int _sqrt_recursion(int n)
 {
-	int i; /* move the declaration to the beginning of the function*/
-
 	if (n < 0)
 		return (-1);
-
-	if (n == 0 || n == 1)
-		return (n);
-
-	/*recursive case*/
-	for (i = 1; i <= n / 2; i++)
-	{
-		if (i * i == n)
-			return (i);
-		else if (i * i > n)
-			return (-1);
-	}
-	return (-1);
+	else
+		return (srqt_loop(n, 0));
 }
