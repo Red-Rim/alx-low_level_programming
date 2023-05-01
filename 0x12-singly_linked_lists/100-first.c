4874a14dd92ef2f65c1c482ea5739a5bafc5b385
Red-Rim <salmaouakhir20@gmail.com>
2023-04-30 19:02:32 -0700

    *

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,11 @@
-#include "lists_t"
+#include "lists.h"
 #include <stdio.h>
-
-void print(void)__attribute__((constructor));
 
 /*Red-Rim*/
 /**
  * print - a function that prints before the main is executed
  */
-void print(void)
+void __attribute__ ((constructor)) before_main()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,19 @@
-/*Red-Rim*/
-global main
-extern printf
-main:
-mov edi, format
-xor eax, eax
-call printf
-mov eax, 0
-ret
-format: db 'Hello, Holberton\n',0
+section .data
+	message db 'Hello, Holberton', 10  ; Declare a null-terminated string to store the message
+
+section .text
+	global _start
+
+_start:
+	; Call printf function to print the message
+	mov eax, 4          ; Set the system call number for write()
+	mov ebx, 1          ; Set the file descriptor for stdout
+	mov ecx, message    ; Set the buffer address to the message string
+	mov edx, 16         ; Set the buffer length to the message string length
+	int 0x80            ; Call the kernel interrupt to perform the system call
+
+	; Exit the program
+	mov eax, 1          ; Set the system call number for exit()
+	xor ebx, ebx        ; Set the exit status to 0
+	int 0x80            ; Call the kernel interrupt to perform the system call
+
