870455b7e1dfc6e22185a1c3e6b3712f1fa88a4f
Red-Rim <salmaouakhir20@gmail.com>
2023-05-02 09:17:14 -0700

    Ama

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,17 +6,17 @@
 /**
  * print_listint - prints all the elements of a listint_t list
  * @h: pointer to the head node of the list
- * Return: the number of nodes in the list
+ * Return: number of nodes in the list
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t c = 0;
+	size_t awdi = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		c++;
+		awdi++;
 	}
-	return (c);
+	return (awdi);
 }
