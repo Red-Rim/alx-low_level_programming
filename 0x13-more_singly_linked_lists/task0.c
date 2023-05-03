4486f83c7938ee023689f559535f6f5d2af5545f
Red-Rim <salmaouakhir20@gmail.com>
2023-05-02 19:20:52 -0700

    Ama

--- 0x13-more_singly_linked_lists/task0.c
+++ 0x13-more_singly_linked_lists/task0.c
@@ -1,22 +1,22 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stddef.h>
+#include <stdio.h>
 
 /*Red-Rim*/
 /**
  * print_listint - prints all the elements of a listint_t list
- * @h: first node
- * Return: numb of nodes in the list
+ * @h: pointer to the head node of the list
+ * Return: number of nodes in the list
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t gang = 0;
+	size_t awdi = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		gang++;
+		awdi++;
 	}
-	return (gang);
+	return (awdi);
 }
