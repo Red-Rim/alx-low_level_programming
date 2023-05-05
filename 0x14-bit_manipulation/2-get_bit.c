4c0600ef8c41f0063c093eb9fc671241521acdfd
Red-Rim <salmaouakhir20@gmail.com>
2023-05-04 18:40:19 -0700

    Baka

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*Red-Rim*/
+/**
+ * get_bit -  returns the value of a bit at a given index
+ * @n: num
+ * @index: the index of the bit to get
+ *
+ * Return: the value of the bit at index, or -1 if an error occurred
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	return ((n >> index) & 1);
+}
+
