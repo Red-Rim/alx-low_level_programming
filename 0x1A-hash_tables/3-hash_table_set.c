c7fa3017e3fb65b242ee8a633651cd4bc9315319
Red-Rim <salmaouakhir20@gmail.com>
2023-07-07 10:33:47 -0700

    .

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table to add/update the key/value to.
+ * @key: The key to add/update.
+ * @value: The value associated with the key.
+ *
+ * Return: 1 if the operation succeeds, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    unsigned long int i;
+    hash_node_t *new_node, *tmp;
+
+    if (ht == NULL || key == NULL || *key == '\0')
+        return (0);
+
+    i = key_i((const unsigned char *)key, ht->size);
+    tmp = ht->array[i];
+
+    while (tmp != NULL)
+    {
+        if (strcmp(tmp->key, key) == 0)
+        {
+            free(tmp->value);
+            tmp->value = strdup(value);
+            return (1);
+        }
+        tmp = tmp->next;
+    }
+
+    new_node = malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+        return (0);
+
+    new_node->key = strdup(key);
+    new_node->value = strdup(value);
+    if (new_node->key == NULL || new_node->value == NULL)
+    {
+        free(new_node->key);
+        free(new_node->value);
+        free(new_node);
+        return (0);
+    }
+
+    new_node->next = ht->array[i];
+    ht->array[i] = new_node;
+
+    return (1);
+}
