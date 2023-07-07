a3728845373e949ff3fc3f0ecc00b591ec8176bb
Red-Rim <salmaouakhir20@gmail.com>
2023-07-07 12:33:36 -0700

    .

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,12 +3,12 @@
 
 /**
  * hash_table_print - Prints the key/value pairs in a hash table.
- * @ht: The hash table to print.
+ * @ht: hash table
  */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
-	int almar = 0
+	int almar = 0;
 	hash_node_t *tmp;
 
 	if (ht == NULL)
@@ -21,7 +21,8 @@ void hash_table_print(const hash_table_t *ht)
 		while (tmp != NULL)
 			{
 			if (almar == 1)
-				printf(", ");
+			{
+				printf(", "); }
 				printf("'%s': '%s'", tmp->key, tmp->value);
 				almar = 1;
 				tmp = tmp->next;
