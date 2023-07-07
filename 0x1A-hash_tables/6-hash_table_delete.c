9bb12bab602f71bd623bc4ed9718b45e2c942abb
Red-Rim <salmaouakhir20@gmail.com>
2023-07-07 13:09:51 -0700

    .

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -18,12 +18,12 @@ void hash_table_delete(hash_table_t *ht)
 	{
 	node = ht->array[i];
 	while (node != NULL)
-        {
-		tmp = node;
-		node = node->next;
-		free(tmp->key);
-		free(tmp->value);
-		free(tmp);
+		{
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
 		}
 	}
 
